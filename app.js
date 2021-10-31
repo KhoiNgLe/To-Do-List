2052b402c2cea25fedd97302205f739c27e0fa8f
KhoiNgLe <91035280+KhoiNgLe@users.noreply.github.com>
2021-10-30 21:07:50 -0400

    Add styles.css & work list
    Update styling + add work list page

--- app.js
+++ app.js
@@ -3,28 +3,56 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-var items =['Buy Food', 'Cook Food', 'Eat Food'];
-
+let items = ['Buy Food', 'Cook Food', 'Eat Food'];
+let workItem = [];
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-  var today = new Date();
-  var option = {
-    weekday:'long',
-    day:'numeric',
-    month:'long'
-}
-  var day = today.toLocaleDateString("en-US",option);
+  let today = new Date();
+  let option = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long'
+  };
+  var day = today.toLocaleDateString("en-US", option);
 
-  res.render('list', {kindOfDay: day, newListItems: items });
+  res.render('list', {
+    listTitle: day,
+    newListItems: items
+  });
 });
 
-app.post('/',function(req,res){
-   var item = req.body.newItem;
-   items.push(item);
-  res.redirect('/');
+app.post('/', function(req, res) {
+
+  let item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItem.push(item);
+    res.redirect('/work');
+  } else {
+    item.push(item);
+    res.redirect('/')
+  }
+
+});
+
+app.get('/work', function(req, res) {
+  res.render('list', {
+    listTitle: 'Work List',
+    newListItems: workItem
+  });
+});
+
+app.post('/work', function(req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect('/work');
 });
 
 app.listen(3000, function() {
